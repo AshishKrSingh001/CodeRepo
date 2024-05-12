1fc1170a8b5c132293a88d497b15d66ea0587c78
AshishKrSingh001 <ashish855116@gmail.com>
2024-05-12 07:46:12 +0530

    New project in Django i. studentstudyportal

---
+++ PYTHON/Django_Project/studentstudyportal/dashboard/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from . forms import *
+
+# Create your views here.
+def home(request):
+    return render(request,"dashboard/home.html")
+
+def notes(request):
+    form = NotesForm()
+    notes = Notes.objects.filter(user=request.user)
+    context = {'notes': notes, 'form': form}
+
+    # Print the form variable for debugging
+    print(form)
+
+    return render(request, "dashboard/notes.html", context)
